98d6415abcaff05b9e9f906c5cc76fa4f322abae
1155194577 <1155194577@link.cuhk.edu.hk>
2024-05-13 18:24:42 +0800

    added alternative message to shopping cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header(props){
         <div className="bg-sky-400 w-full h-19 flex justify-between">
         <h1 className="px-2 hover:underline underline-offset-2 font-bold" onClick={props.home}>Website Logos</h1>
         <div className='px-3' onClick={props.shoppingcart}>
-        <img className="w-5 h-5 hover:w-6 hover:h-6" src="src/assets/cart-shopping-svgrepo-com.svg"></img>
+        <img className="w-5 h-5 hover:w-6 hover:h-6" src="src/assets/cart-shopping-svgrepo-com.svg" alt="Shopping Cart"></img>
         </div>
         </div>
     )     
