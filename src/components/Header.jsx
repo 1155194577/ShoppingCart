49b2d72d997444505bcff2e6c4df0eac2d8994b7
1155194577 <1155194577@link.cuhk.edu.hk>
2024-05-13 16:52:32 +0800

    test messages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 function Header(props){
     return (
         <div className="bg-green-600 w-full h-18 flex justify-between">
-        <h1 className="px-2" onClick={props.home}>Website Logo</h1>
+        <h1 className="px-2" onClick={props.home}>Website Logos</h1>
         <h1 className="px-2" onClick={props.shoppingcart}>Shopping Cart</h1>
         </div>
     )     
