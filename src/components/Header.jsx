545510f9db47e6aec36ebef9f6968bab9a0786e2
1155194577 <1155194577@link.cuhk.edu.hk>
2024-05-13 18:22:34 +0800

    changed shopping cart to icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 function Header(props){
     return (
-        <div className="bg-green-600 w-full h-18 flex justify-between">
-        <h1 className="px-2" onClick={props.home}>Website Logos</h1>
-        <h1 className="px-2" onClick={props.shoppingcart}>Shopping Cart</h1>
+        <div className="bg-sky-400 w-full h-19 flex justify-between">
+        <h1 className="px-2 hover:underline underline-offset-2 font-bold" onClick={props.home}>Website Logos</h1>
+        <div className='px-3' onClick={props.shoppingcart}>
+        <img className="w-5 h-5 hover:w-6 hover:h-6" src="src/assets/cart-shopping-svgrepo-com.svg"></img>
+        </div>
         </div>
     )     
 
